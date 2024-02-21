fad5a066efa5c4832566664c55bf4c282c7db303
perfer95 <perfer95@outlook.com>
2024-02-21 08:57:20 +0000

    5 printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Author: perfer
+ * Description: using printf
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main function of the code
+ * Description: Prints a message on the screen using printf
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
