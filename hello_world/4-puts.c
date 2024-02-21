5bd0a7f86ca5afba5e248f2e83ae77eb2472fa6c
perfer95 <perfer95@outlook.com>
2024-02-21 08:01:07 +0000

    4 puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/**
+ * File: 4-puts.c
+ */
+
+#include <stdio.h>
+
+/**
+ * Prints a message and return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
