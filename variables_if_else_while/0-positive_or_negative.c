8e9d0bbf93ef156ce397802777ae2b795854828c
perfer95 <perfer95@outlook.com>
2024-02-22 21:33:54 +0000

    0 if

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Author: perfer
+ * Descirpiton: This program will assign a random number
+ * to the variable n each time it is executed. Complete
+ * the source code in order to print whether the number
+ * stored in the variable n is positive or negative.
+ */
+
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - main function of the code
+ *
+ * Descirpiton: Generate a random number and clasify
+ * as positive, negative or zero.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+		printf("%i is zero\n");
+
+	return (0);
+}
