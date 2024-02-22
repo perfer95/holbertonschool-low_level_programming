4fb4622389ca181cb03b1c99d8f88e17880c7124
perfer95 <perfer95@outlook.com>
2024-02-22 21:53:21 +0000

    changin %i

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -32,10 +32,10 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 	else
-		printf("%d is zero\n", n);
+		printf("%i is zero\n", n);
 
 	return (0);
 }
