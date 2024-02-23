5d897d41596f215c2740f8b064e0adb5235996ac
perfer95 <perfer95@outlook.com>
2024-02-23 22:51:55 +0000

    7 Last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_last_digit.c
+ * Author: perfer
+ * Description: function that prints the
+ * last digit of a number.
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - computes
+ * @n: The number to filter
+ * Description: return the value of the last digit
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	_putchar(last + '0');
+
+	return (last);
+}
