3af53ff838d44189624c84760fb13afb395ca30e
perfer95 <perfer95@outlook.com>
2024-02-23 20:58:28 +0000

    0 function alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+/*
+ * File: 1-alphabet.c
+ * Author: perfer
+ * Description: function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - printer
+ * Description: prints alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n);
+}
