8a5c5cb892f3f84ae2660c9dd38ea911f1857f51
perfer95 <perfer95@outlook.com>
2024-02-23 22:34:54 +0000

    6 abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/*
+ * File: 6-abs.c
+ * Author: perfer
+ * Description: Function that computes the absolute value of an integer.
+ */
+
+#include "main.h"
+
+/**
+ * _abs - computes
+ * @n: The number to tranform
+ * Description: change the absolute value
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
