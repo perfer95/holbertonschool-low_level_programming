05f2aa82f84efa8033980d1e4d4c6256a91f9240
perfer95 <perfer95@outlook.com>
2024-02-23 22:19:03 +0000

    4 signos

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+/*
+ * File: 5-sign.c
+ * Author: perfer
+ * Description: Function that prints the sign of a number.
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - checker
+ * @n: The number to check
+ * Description: check the sign of the number
+ * Return: 1 if is greater than zero, 0 if is zero
+ * -1 if is less than zero
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('+');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
