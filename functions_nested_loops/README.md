4168cf77ca98dd93aa531664baba9c326a24146f
perfer95 <perfer95@outlook.com>
2024-02-23 18:47:11 +0000

    0 funciton

---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Tasks C - Functions, nested loops
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/*
+ * File: main.h
+ * Author: perfer
+ * Description: Header with all the prototypes of the function
+ */
+
+int _putchar(char c);
