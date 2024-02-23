f254201900b5923c4035e0d89f7058c79df55a5e
perfer95 <perfer95@outlook.com>
2024-02-23 06:03:28 +0000

    9 cast

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+/*
+ * File: 9-print_comb.c
+ * Author: perfer
+ * Description: Program that prints all possible
+ * combinations of single-digit numbers.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main function of the code
+ * Description: Prints all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int numComb = 0;
+
+	while (numComb <= 9)
+	{
+		putchar((char)numComb);
+
+		if (numComb != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		numComb++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
