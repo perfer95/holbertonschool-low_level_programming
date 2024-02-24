a55b040119e6f44102f4ebe3619f8c39e0fb1958
perfer95 <perfer95@outlook.com>
2024-02-24 23:51:08 +0000

    11 combination

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+/*
+ * File: 101-print_comb4.c
+ * Author: perfer
+ * Description: program that prints all
+ * possible different combinations of three digits.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main function of the code
+ * Description: Prints all possible combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int numHund = 0;
+	int numDec = 1;
+	int numUnit = 2;
+
+	while (numHund < 8)
+	{
+		while (numDec < 9)
+		{
+			while (numUnit < 10)
+			{
+				putchar(numHund + '0');
+				putchar(numDec + '0');
+				putchar(numUnit + '0');
+
+				if (numHund != 7 || numDec != 8 || numUnit != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				numUnit++;
+			}
+			numUnit = numDec + 1;
+			numDec++;
+		}
+		numDec = numHund + 1;
+		numDec++;
+	}
+	putchar('\n');
+
+	return (0);
+}
