3bfe876322d5b889e21313129ce108d6dad8be17
perfer95 <perfer95@outlook.com>
2024-02-24 08:04:14 +0000

    9 while nested

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,68 @@
+/*
+ * File: 9-times_table.c
+ * Author: perfer
+ * Description: function that prints
+ * the 9 times table, starting with 0.
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - printer
+ * Description: print the table
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int num;
+
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			num = i * j;
+
+			if (j == 0)
+			{
+				_putchar(num + '0');
+				_putchar(',');
+                                _putchar(' ');
+			}
+			else if (num < 10 && j != 9)
+			{
+				_putchar(' ');
+				_putchar(num + '0');
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			else if (num >= 10 && j != 9)
+			{
+				_putchar((num / 10) + '0'); /*First number*/
+				_putchar((num % 10) + '0');/*Last number*/
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (num < 10 && j == 9)
+			{
+				_putchar(' ');
+                                _putchar(num + '0');
+				_putchar('\n');
+			}
+			else if (num >= 10 && j == 9)
+			{
+				_putchar((num / 10) + '0'); /*First number*/
+                                _putchar((num % 10) + '0');/*Last number*/
+				_putchar('\n');
+			}
+
+			j++;
+		}
+
+		j = 0;
+		i++;
+	}
+}
