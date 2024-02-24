adef3945d2175153f3c4581b119cdd20504a94d0
perfer95 <perfer95@outlook.com>
2024-02-24 22:55:32 +0000

    10 combination

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+/*
+ * File: 100-print_comb3.c
+ * Author: perfer
+ * Description: program that prints all
+ * possible different combinations of two digits.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main function of the code
+ * Description: Prints all possible combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int numDec = 0;
+	int numUnit = 1;
+
+	while (numDec < 9)
+	{
+		while (numUnit < 10)
+		{
+			putchar(numDec + '0');
+			putchar(numUnit + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+			numUnit++;
+		}
+		numDec++;
+	}
+	putchar('\n');
+
+	return (0);
+}
