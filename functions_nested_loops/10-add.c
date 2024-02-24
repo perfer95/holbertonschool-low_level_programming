99bd681bc3e048b77484b9777fbca0b9a5693a57
perfer95 <perfer95@outlook.com>
2024-02-24 09:14:39 +0000

    10 addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+/*
+ * File: 10-add.c
+ * Author: perfer
+ * Description: function that adds
+ * two integers and returns the result.
+ */
+#include "main.h"
+
+/**
+ * add - summation
+ * @a: first number
+ * @b: second number
+ * Description: add two numbers
+ * Return: a int
+ */
+int add(int a, int b)
+{
+	int addition = a + b;
+
+	return (addition);
+}
