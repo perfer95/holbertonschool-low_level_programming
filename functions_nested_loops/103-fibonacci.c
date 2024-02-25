24d60725258b528f1a757e2c135bcf269e186135
perfer95 <perfer95@outlook.com>
2024-02-25 02:42:40 +0000

    15 fibinacci pair sum

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+/*
+ * File: 103-fibonacci.c
+ * Author: perfer
+ * Description: write a program that finds and prints
+ * the sum of the even-valued terms, followed by a new line.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main code
+ * Description:  print the sum of pairs
+ * Return: void
+ */
+int main(void)
+{
+	unsigned long sum = 0;
+	unsigned long fn;
+	unsigned long n1 = 0;
+	unsigned long n2 = 1;
+
+	while (fn <= 4000000)
+	{
+		fn = n1 + n2;
+
+		if ((fn % 2) == 0)
+		{
+			sum = sum + fn;
+		}
+
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
