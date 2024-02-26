7875a7fd7f0ee226da6fd3cbcfb04098ba8d3dba
perfer95 <perfer95@outlook.com>
2024-02-26 23:34:08 +0000

    4 print 0-9 except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Author: perfer
+ */
+
+#include "main.h"
+
+/**
+ * print_must_numbers - print the number 0 to 9 except 2 and 4
+ * return: void just print
+ */
+void print_must_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
