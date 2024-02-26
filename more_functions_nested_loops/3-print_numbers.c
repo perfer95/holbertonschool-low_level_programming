c1a4242229461da680f2c1a6b30c2803c6f39e67
perfer95 <perfer95@outlook.com>
2024-02-26 23:27:06 +0000

    3 print 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-print_numbers.c
+ * Author: perfer
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - print the number 0 to 9
+ * return: void just print
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar(i + '0');
+	}
+}
+
