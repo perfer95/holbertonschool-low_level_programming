5bd1f873df54d784482b2d072513c45b1fc9f660
perfer95 <perfer95@outlook.com>
2024-02-26 23:16:28 +0000

    multiplies

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Author: perfer
+ * Description: function that multiplies two integers
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplier
+ * Description: just multiplies
+ * @a: digit one
+ * @b: digit two
+ * Return: int with the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
