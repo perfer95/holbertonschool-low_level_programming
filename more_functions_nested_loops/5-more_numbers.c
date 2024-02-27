69fc118e9f30f18a1c6350ed5ad17affdb050098
perfer95 <perfer95@outlook.com>
2024-02-27 00:55:09 +0000

    5 more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,18 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < 15; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if (i > 10)
+		for (i = 0; i < 15; i++)
 		{
-			_putchar((i / 10) + '0');
+			if (i > 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
-		_putchar((i % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
