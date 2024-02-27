7ee2bf112b1c27b99ae8874067f7c250c698ceaf
perfer95 <perfer95@outlook.com>
2024-02-27 01:06:42 +0000

    6 print _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+/*
+ * File: 6-print_line.c
+ * Author: perfer
+ */
+
+#include "main.h"
+
+/**
+ * print_line -function that draws a straight line in the terminal.
+ * @n: number of times to print _
+ * return: void just print
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0 && n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n);
+}
