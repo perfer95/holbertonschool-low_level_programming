c018443ff6e361471a9df0ca1d35a747227dc8fe
perfer95 <perfer95@outlook.com>
2024-02-27 02:56:05 +0000

    9 Fizz-Buzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+/*
+ * File: 9-fizz_buzz.c
+ * Author: perfer
+ */
+
+#include <stdio.h>
+
+/**
+ * main - function that prints a square,
+ * followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
