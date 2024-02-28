edcfd90e6f3b53204b2bb6f7f39b83d99ab29ce7
perfer95 <perfer95@outlook.com>
2024-02-28 23:15:32 +0000

    5 reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+/*
+* File: 5-rev_string.c
+*/
+
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: string to reverse
+ * Return: empty
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char *inv = *s
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	k = i - 1;
+
+	for (j = 0; j <= (i - 1); j++)
+	{
+		*s[j] = *inv[k];	
+		k--;
+	}
+
+	_putchar('\n');
+}
