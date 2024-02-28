38e37e3406a7d4d8e64343c523b55c8adbf197c9
perfer95 <perfer95@outlook.com>
2024-02-28 22:31:32 +0000

    2 size string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-strlen.c
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: variable string to check the size
+ * Return: an integer with the size
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
+}
