23eb43360380d1edb0ba09e7a233f4aa6139ca5c
perfer95 <perfer95@outlook.com>
2024-02-28 21:31:53 +0000

    0 pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: 0-reset_to_98.c
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - change a value o a variable with pointers
+ * @n: variable integer pointer
+ * return: empty
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Directory to array an string section
