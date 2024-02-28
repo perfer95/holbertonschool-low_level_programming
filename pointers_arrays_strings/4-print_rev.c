4f0cbe461ce71f7cc0d37f40b3b0ec337fbfe461
perfer95 <perfer95@outlook.com>
2024-02-28 22:57:08 +0000

    4 pritn reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_rev.c
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, followed by a new line
+ * @s: string to print
+ * Return: empty
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
