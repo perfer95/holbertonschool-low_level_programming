fc77a373315057616fe0d0af825c9e367e59320b
perfer95 <perfer95@outlook.com>
2024-03-01 07:49:06 +0000

    8 print array integer

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,17 +16,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n !=  0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (i == (n - 1))
+			if (i != (n - 1))
 			{
-				printf("%d\n", a[i]);
+				printf("%d, ", a[i]);
 			}
 			else
 			{
-				printf("%d, ", a[i]);
+				printg("%d\n", a[i]);
 			}
 		}
 	}
