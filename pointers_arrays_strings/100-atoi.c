25ed0115d3001475cd1c39b5b986fdfefba9eee4
perfer95 <perfer95@outlook.com>
2024-03-01 22:29:31 +0000

    9 filter

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: string to convert
+ * Return: int 
+ */
+int _atoi(char *s)
+{
+	int i;
+	int num = 0;
+	int sign = 1;
+	int sysNum = 10;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (s[i] - '0') + (num * sysNum);
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+		}
+
+	}
+	return (num * sign);
+}
