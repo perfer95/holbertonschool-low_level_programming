8d3c72d90f2ad5fe082d67291366b7260d6a4e2d
perfer95 <perfer95@outlook.com>
2024-03-01 07:24:57 +0000

    8 print array integer

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+/*
+* File: 8-print_array.c
+*/
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements
+ * of an array of integers
+ * @a: array of integers
+ * @n: number of elements to print
+ * Return: empty
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+}
