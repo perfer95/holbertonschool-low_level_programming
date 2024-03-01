0f1a80c51ceb1515ff767ab54a8a6a8f47950ea2
perfer95 <perfer95@outlook.com>
2024-03-01 07:11:52 +0000

    7 print haf

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -24,14 +24,14 @@ void puts_half(char *str)
 
 	if ((i % 2) == 0)
 	{
-		for (j = (size / 2); j < size; j++)
+		for (j = (size / 2); str[j] != '\0'; j++)
 		{
 			_putchar(str[j]);
 		}
 	}
 	else
 	{
-		for (j = ((size - 1) / 2); j < size; j++)
+		for (j = (((size - 1) / 2) + 1); str[j] != '\0'; j++)
 		{
 			_putchar(str[j]);
 		}
