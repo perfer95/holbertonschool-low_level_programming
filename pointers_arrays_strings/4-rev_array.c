ca62a80512284e308dfd2604568d231ad710ed16
perfer95 <perfer95@outlook.com>
2024-03-02 22:16:42 +0000

    4 reverse

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,13 @@
 void reverse_array(int *a, int n)
 {
 	int j, aux;
+	n = n - 1;
 
 	for (j = 0; j < (n / 2); j++)
 	{
 		aux = a[j];
-		a[j] = a[n - 1];
-		a[n - 1] = aux;
+		a[j] = [n];
+		a[n] = aux;
 		n--;
 	}
 }
