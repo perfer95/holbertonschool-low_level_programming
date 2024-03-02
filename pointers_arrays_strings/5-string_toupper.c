b6cea7416561185878b5b8a294240a9d0ae2d1ef
perfer95 <perfer95@outlook.com>
2024-03-02 23:02:34 +0000

    5 to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that reverses the content
+ * of an array of integers
+ * @str: string to check
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32; /*diference in low an upper*/
+		}
+		i++;
+	}
+}
