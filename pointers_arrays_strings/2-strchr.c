4269edf58b18971d06887f9dd1a622300b18d227
perfer95 <perfer95@outlook.com>
2024-03-06 07:59:54 +0000

    2 strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr -  function that locates a character in a string.
+ * @s: string to check
+ * @c: char to search
+ * Return: a pointer of char type
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+
+			return (s + i);
+		}
+		i++;
+	}
+
+	if (s[i] == '\0')
+	{
+		return (s + i);
+	}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,5 +25,6 @@ char *leet(char *);
 /*Section 3*/
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
