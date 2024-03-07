e725534211c93c42178cbdf4e2afd21023dc6604
perfer95 <perfer95@outlook.com>
2024-03-07 08:01:36 +0000

    8 print sumdiag

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int diag1 = 0;
 	int diag2 = 0;
 
-	for (i = 0, j = 0; i < 8; i++, j++)
+	for (i = 0; i < (size * size); i = (i + size + 1))
 	{
-		diag1 = diag1 + a[i][j];
+		diag1 = diag1 + a[i];
 	}
-	for (i = size - 1, j = size - 1; i >= 0; i--, j--)
+	for (i = (size - 1); i >= 0 ; i = (i - size - 1))
 	{
-		diag2 = diag2 + a[i][j];
+		diag2 = diag2 + a[i];
 	}
 	printf("%d, %d\n", diag1, diag2);
 }
