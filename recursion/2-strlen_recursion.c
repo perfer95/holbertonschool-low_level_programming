ddc4fcc95cc3bfd3c23aa0ee7267077d2467255c
perfer95 <perfer95@outlook.com>
2024-03-08 21:21:13 +0000

    2 length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -  function that returns the length of a string.
+ * @s: string to measure
+ * Return: size of s
+ */
+int _strlen_recursion(char *s)
+{
+	int size = 0;
+
+	if (*s != '\0')
+	{
+		size = _strlen_recursion(s + 1) + 1
+	}
+
+	return (size);
+}
