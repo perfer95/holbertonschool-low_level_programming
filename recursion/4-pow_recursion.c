9e3d2a7391845f34e75a15b68be8df3097235fbd
perfer95 <perfer95@outlook.com>
2024-03-08 22:22:21 +0000

    4 power

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y.
+ * _pow_recursion - function that returns the value of
+ * x raised to the power of y.
  * @x: int base
  * @y: int exponent
  * Return: result
