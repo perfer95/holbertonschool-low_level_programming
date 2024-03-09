d9f912be8df5e1d0b2a2578a80389f5c28875e39
perfer95 <perfer95@outlook.com>
2024-03-09 07:43:17 +0000

    0 argc anf argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,8 +3,10 @@
 #include "main.h"
 
 /**
- * main -
- *
+ * main - program that prints its name, followed by a new line.
+ * @argc: counter
+ * @argv: array
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
