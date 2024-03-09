d27acc3525af58cfedf58a0e92c4a7fc57c717f4
perfer95 <perfer95@outlook.com>
2024-03-09 08:01:54 +0000

    2 print elements

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: counter
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
