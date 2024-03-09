cbdaafce71f83c8227e2e528260775b6792b90b2
perfer95 <perfer95@outlook.com>
2024-03-09 07:48:01 +0000

    1 argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - program that prints the number of arguments
+ *passed into it.
+ * @argc: counter
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void) argv;
+
+	return (0);
+}
