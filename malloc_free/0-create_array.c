e85facb5a53aa7962294721137db47561d44aaa9
perfer95 <perfer95@outlook.com>
2024-03-13 22:43:50 +0000

    0 malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,6 +15,11 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(sizeof(char) * size);
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		if (array == NULL)
