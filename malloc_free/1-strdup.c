7ba2c2946729a3cabeadbc8aab87b4f194732d09
perfer95 <perfer95@outlook.com>
2024-03-14 08:17:03 +0000

    1 duplicate with malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,17 +14,19 @@ char *_strdup(char *str)
 	int size = 0;
 	char *str2 = malloc(sizeof(str));
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+
+	str2 = malloc(sizeof(char) * (size + 1));
+
 	if (str2 == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		for (i = 0; str[i] != '\0'; i++)
-		{
-			size++;
-		}
-
 		for (i = 0; i <= size; i++)
 		{
 			str2[i] = str[i];
