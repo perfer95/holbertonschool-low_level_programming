e8304b316eb98031acb565e43cfa2c3073280d2a
perfer95 <perfer95@outlook.com>
2024-03-15 08:26:22 +0000

    3 array 2 dimension malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -30,6 +30,11 @@ int **alloc_grid(int width, int height)
 		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
 		{
+			while (i >= 0)
+			{
+				free(array[i]);
+			}
+			free(array[i]);
 			return (NULL);
 		}
 	}
