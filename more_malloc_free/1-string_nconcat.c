9889bb45abb05ea1c0ac5a6b999f410b69c463c7
perfer95 <perfer95@outlook.com>
2024-03-16 01:16:32 +0000

    1 concact

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		concat[i] = s1[i];
 	}
-	for (j = 0; s2[j] != '\0'; j++, i++)
+	for (j = 0; j < n; j++, i++)
 	{
 		concat[i] = s2[j];
 	}
