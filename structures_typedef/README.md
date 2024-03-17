a06034ebfd51b4775a9e242be21829312ab7b39a
perfer95 <perfer95@outlook.com>
2024-03-17 00:09:07 +0000

    0 struc

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Topic structures and typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data
+ * @name: dog´sname
+ * @age: dog�s age
+ * @owner: dog´ owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
