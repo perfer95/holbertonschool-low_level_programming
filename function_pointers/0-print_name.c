7d050f7617e428f1b8a31995585c2f6185c06a72
perfer95 <perfer95@outlook.com>
2024-03-21 06:50:00 +0000

    0 pointer function

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: string with the name
+ * @f: pointer function
+ * Return: void just print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+Topic Funcition pointers
