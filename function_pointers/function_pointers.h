bae575ae49c8364691cb0f1ee19995276f428a7b
perfer95 <perfer95@outlook.com>
2024-03-21 07:09:15 +0000

    1 pointer function

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif
