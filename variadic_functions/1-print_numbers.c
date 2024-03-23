c5f36fa6e334ec5262569e6a07c3913187539178
perfer95 <perfer95@outlook.com>
2024-03-23 21:48:05 +0000

    1 variable arguments

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line.
+ * @separator: sprintg to be printed between numbers
+ * @n: number of arguments
+ * Return: empty just print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args; /*Firs variable*/
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator == NULL)
+		{
+			printf(" ");
+		}
+		else if (i == n - 1)
+		{
+			print("\n");
+		}
+		else
+		{
+			printf("%c ", *separator);
+		}
+	}
+	va_end(args);
+}
