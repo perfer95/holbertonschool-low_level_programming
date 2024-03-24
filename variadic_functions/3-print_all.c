5c2bf53b73d7b39576dfb2bcceba04f9bf69866f
perfer95 <perfer95@outlook.com>
2024-03-24 01:08:58 +0000

    3 print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include "variadic_functions.h"
+
+void p_char(char *separator, va_list args);
+void p_integer(char *separator, va_list args);
+void p_float(char *separator, va_list args);
+void p_char_ptr(char *separator, va_list args);
+
+
+/**
+ * print_all - function that prints anything.
+ * @format:  list of types of arguments passed to the function
+ * Return: empty just print
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	va_list args;
+	s_type type[] = {
+		{ "c", p_char },
+		{ "i", p_integer },
+		{ "f", p_float },
+		{ "s", p_char_ptr }
+	};
+	char *separator = "";
+
+	va_start(args, format);
+
+	while (format[i] && format != NULL)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *type[j].identifier)
+			{
+				type[j].f(separator, args);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
+
+/**
+  * p_char - Prints char
+  * @separator: separator
+  * @args: Arguments
+  * Return: empty
+  */
+void p_char(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+
+/**
+  * p_integer - Prints integers
+  * @separator: separator
+  * @args: arguments
+  * Return: empty
+  */
+void p_integer(char *separator, va_list args)
+{
+	printf("%s%i", separator, va_arg(args, int));
+}
+
+/**
+  * p_float - Prints float
+  * @separator: separator
+  * @args: arguments
+  * Return: empty
+  */
+void p_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+
+/**
+  * p_char_ptr - Prints char pointer
+  * @separator: separator
+  * @args: arguments
+  * Return: empty
+  */
+void p_char_ptr(char *separator, va_list args)
+{
+	char *arg = va_arg(args, char *);
+
+	if (arg == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+	printf("%s%s", separator, arg);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,9 +5,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
+typedef struct types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} s_type;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
