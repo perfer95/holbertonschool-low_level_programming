6837aa5d28d54a3e9f8c872509035fee8832befc
perfer95 <perfer95@outlook.com>
2024-04-06 08:28:17 +0000

    0 dobly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <list.h>
+
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * @h: doulbe linked list to print
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Activities C - Doubly linked lists
