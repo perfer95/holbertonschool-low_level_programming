0a38b5268b664348d528af99b8469a7fee389eb0
perfer95 <perfer95@outlook.com>
2024-04-06 09:17:37 +0000

    6 sum

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of all the data
+ * (n) of a dlistint_t linked list.
+ * @head: pointer double link list to check
+ * Return: a int number.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *auxi = head;
+
+	while (auxi != NULL)
+	{
+		auxi = auxi + cursor->n;
+		auxi = auxi->next;
+	}
+
+	return (sum);
+}
