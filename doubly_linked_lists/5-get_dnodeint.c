1f5fb8f75be027ddaeac8670ad030d881b17a69f
perfer95 <perfer95@outlook.com>
2024-04-06 09:12:04 +0000

    5 index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that returns the nth node of
+ * a dlistint_t linked list.
+ * @head: pointer double link list to check
+ * @index: integer with th position
+ * Return: the node asked
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	while ((index != 0) && (head != NULL))
+	{
+		head = head->next;
+		index--;
+	}
+
+	if ((index != 0) && (head != NULL))
+	{
+		return (head);
+	}
+
+	return (NULL);
+}
