20274aa1bd3cc6074155b197e9a3898301ff09d8
perfer95 <perfer95@outlook.com>
2024-04-06 07:24:29 +0000

    2 add new node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: struct
+ * @str: string to add
+ * Return:  the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *auxi;
+
+	auxi = malloc(sizeof(list_t));
+
+	if (auxi == NULL)
+	{
+		return (NULL);
+	}
+
+	temp->str = strdup(str);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	temp->len = i;
+	temp->next = *head;
+	*head = auxi;
+	return (auxi);
+}
