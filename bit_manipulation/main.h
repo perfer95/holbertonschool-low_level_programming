df99614b3e6ca29746452aa9f76d111cdb5ef56d
perfer95 <perfer95@outlook.com>
2024-04-07 01:47:59 +0000

    4 clear bit

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -9,6 +9,7 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 /*Auxiliar functions*/
 int _pow_recursion(int x, int y);
